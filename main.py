db00679056ba7f9a5a5f5863a672d1655ed389d5
Vaibhav Khurana <62386056+vaibhavcode@users.noreply.github.com>
2024-07-15 17:41:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# This code is based on the following example:
+# https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
+
+import os
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.watching, name="BitbyBit v5.0 😻"))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!init 1'):
+        embedVar = discord.Embed(title="Title", description="Desc", color=0x00fff0)
+        embedVar.add_field(name="Field1", value="hi", inline=False)
+        embedVar.add_field(name="Field2", value="hi2", inline=False)
+        await message.channel.send(embed=embedVar)
+
+
+
+client.run(os.getenv('TOKEN'))
