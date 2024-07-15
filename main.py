81e9f7546c3a4e39790bd95077b7af5cabca7764
Vaibhav Khurana <62386056+vaibhavcode@users.noreply.github.com>
2024-07-15 15:51:57 +0000

    33

--- main.py
+++ main.py
@@ -22,14 +22,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+
 @client.event
 async def on_message(message):
     if message.content.startswith('!init 1'):
-        embedVar = discord.Embed(title="Title", description="Desc", color=0x00fff0)
+        embedVar = discord.Embed(
+            title="Title", description="Desc", color=0x00fff0)
         embedVar.add_field(name="Field1", value="hi", inline=False)
         embedVar.add_field(name="Field2", value="hi2", inline=False)
         await message.channel.send(embed=embedVar)
 
 
-
+keep_alive()
 client.run(os.getenv('TOKEN'))
